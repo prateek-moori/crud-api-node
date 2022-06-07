eabd5a80c7673833522eaab49affe89cc8f9f83a
prateek-moori <f20170668@pilani.bits-pilani.ac.in>
2022-06-07 17:35:34 +0530

    adding routes and methods

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,46 @@
 import express from "express"
+import { v4 as uuidv4 } from 'uuid';
 
-const routes= express.Router();
+const router= express.Router();
+
+let users=[]
+
+
+router.get("/",(req,res)=>{
+    console.log(users);
+    res.send(users);
+})
+
+router.post("/",(req,res)=>{
+    console.log("ROUTE REACHED HERE");
+    console.log(req.body);
+    const user= req.body;
+
+    users.push({...user,id:uuidv4()});
+
+    res.send(users);
+})
+
+router.get("/:id",(req,res)=>{
+    const { id }= req.params;
+    const user= users.find((element)=>element.id === id);
+    res.send(user);
+})
+
+router.delete("/:id",(req,res)=>{
+    const { id }= req.params;
+    console.log("==============");
+    console.log(users);
+    let tempo=[];
+   for(let user of users){ 
+       if(user.id !== id)
+       {
+            tempo.push(user);
+       }
+   }
+    console.log(tempo);
+    users= tempo;
+    res.send(`user with ${id} got dleted`);
+})
+
+export default router;
